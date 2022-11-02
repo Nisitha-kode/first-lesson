36d295730c4d21d63df11a55567e076f4b1a7f69
NisithaPrakash <nisitha.kodehode@gmail.com>
2022-11-02 14:00:34 +0100

    Code in Header.jsx

---
+++ src/Components/Header.jsx
@@ -0,0 +1,16 @@
+/*import React, { Component } from 'react'
+class Header extends Component {
+render () {
+return (
+<div>
+<h1>HEllo Welcome Everyone</h1>
+<button>Increment</button>
+</div>
+)
+}
+}*/
+
+const Header = probs => {
+  return <h1>{Header}</h1>
+}
+export default Header
